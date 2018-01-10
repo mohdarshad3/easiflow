06cdf2cab94ee21f7c70a3244fac6fb9fd6ea259
Alok Kushwaha <dsmail.alok@gmail.com>
2018-01-10 12:33:46 +0530

    commit all

---
+++ src/app/theme/pages/default/form-builder/form-builder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: '.m-wrapper',
+    templateUrl: './form-builder.component.html',
+    encapsulation: ViewEncapsulation.None,
+})
+export class FormBuilderComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+}
