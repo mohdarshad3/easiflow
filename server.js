b2a48862805aa6c20b7b24bb2d13954f2926f3bc
dreamsstudios <vikas@dreamsstudios.org>
2018-01-18 15:27:41 +0530

    Colsls

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname+'/dist'));
-app.listen(process.env.PORT || 8080);
+console.log("Hello World");
+app.listen(process.env.PORT || 4000);
