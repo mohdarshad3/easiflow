e5a690d94860ec6a7687adf497a4dd490e9dc610
dreamsstudios <vikas@dreamsstudios.org>
2018-01-18 16:30:49 +0530

    sdddfsdf

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-app.use(express.static(__dirname+'/'));
+app.use(express.static(__dirname+'/dist'));
 console.log("Hello World");
 app.listen(process.env.PORT || 8080);
